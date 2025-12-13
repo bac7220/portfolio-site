22193d64f3b06818c98fdad98821e1cf5cdc7c17
bac7220 <bacbac7220@gmail.com>
2025-12-13 17:32:08 +0900

    init:VueとmicroCMSを用いたポートフォリオ作成

---
+++ src/lib/microcms.js
@@ -0,0 +1,6 @@
+import { createClient } from 'microcms-js-sdk';
+
+export const client = createClient({
+  serviceDomain: 'bac7220',
+  apiKey: 'fFeg3YNjRV5j99zAiJhiTf2cYdEX8V92vh0w',
+});
---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue';
+import { client } from '../lib/microcms.js';
+import { ref, onMounted } from 'vue';
+
+const works = ref([]);
+onMounted(async () => {
+  const data = await client.get({ endpoint: 'works' });
+  works.value = data.contents;
+})
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+    
+  </main>
+</template>
