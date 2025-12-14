0ce4e0e8406f718223ce165fb83fce7fa53ca0f4
bac7220 <bacbac7220@gmail.com>
2025-12-14 17:22:06 +0900

    詳細ページ作成。全体をv-ifで囲うことで途中の読み込み漏れエラーをなくす

--- src/views/WorkDetailView.vue
+++ src/views/WorkDetailView.vue
@@ -1,7 +1,7 @@
 <script setup>
-import { client } from '../lib/microcms.js';
-import { ref, onMounted } from 'vue';
-import { useRoute } from 'vue-router';
+import { client } from "../lib/microcms.js";
+import { ref, onMounted } from "vue";
+import { useRoute } from "vue-router";
 
 const route = useRoute();
 const work = ref(null);
@@ -9,15 +9,27 @@ const work = ref(null);
 onMounted(async () => {
   const slug = route.params.slug;
   const data = await client.get({
-    endpoint: 'works',
-    queries: { filters: `work_slug[equals]${slug}` }
+    endpoint: "works",
+    queries: { filters: `work_slug[equals]${slug}` },
   });
   work.value = data.contents[0];
-})
+});
 </script>
 
 <template>
   <main>
-    <h1>{{ work?.work_title}}</h1>
+    <div v-if="work">
+      <h1>{{ work.work_title }}</h1>
+      <div style="width: 100%; aspect-ratio: 16/ 9; overflow: hidden; border-radius: 10px">
+        <img
+          :src="work.work_thumbnail?.url"
+          :alt="work.work_title"
+          style="width: 100%; height: auto"
+        />
+      </div>
+      <p>{{ work.work_description }}</p>
+      <div v-html="work.work_body"></div>
+    </div>
   </main>
 </template>
+
