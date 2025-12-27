7d3160cba68c7463877583d5a81db642fa00c677
bac7220 <bacbac7220@gmail.com>
2025-12-27 19:16:33 +0900

    microCMSからプロフィール情報を呼び出し設定

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,42 @@
+<script setup>
+import { client } from "../lib/microcms.js";
+import { ref, onMounted, } from "vue";
+
+const profile = ref(null);
+const isLoading = ref(true);
+
+onMounted(async () => {
+  try {
+    const data = await client.get({
+      endpoint: "profile",
+    });
+    profile.value = data;
+  } catch (error) {
+    console.error("データの取得に失敗しました", error);
+  } finally {
+    isLoading.value = false;
+  }
+});
+
+</script>
+
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h2 class="about">This is an about page</h2>
+    <pre>{{ profile }}</pre>
+    <div v-if="profile" class="profile-wrapper">
+      <div>{{ profile?.profile_name }}</div>
+      <div class="profile-card" v-html="profile?.profile_intro"></div>
+      <div class="profile-card">{{ profile?.profile_skill }}</div>
+      <div class="profile-card">{{ profile?.profile_tools }}</div>
+      <div class="profile-card" v-html="profile?.profile_history"></div>
+      <div class="profile-card" v-html="profile?.profile_activity"></div>
+      <div class="profile-card" v-html="profile?.profile_hobby"></div>
+      <div class="profile-card" v-html="profile?.profile_belief"></div>
+      <div class="profile-img"><img :src="profile?.profile_img.url" alt=""></div>
+    </div>
+    <div v-else-if="isLoading" class="profile-loading">now Loading...</div>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
