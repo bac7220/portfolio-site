71e224eed20197148315d80ef269a2f5bbf7cccc
bac7220 <bacbac7220@gmail.com>
2025-12-27 19:55:26 +0900

    AboutView.vue:スクロールに合わせてプロフィールカードが左右に移動 飛び込むように表示されるアニメーションの実装

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,10 @@
 <script setup>
 import { client } from "../lib/microcms.js";
-import { ref, onMounted, } from "vue";
+import { ref, onMounted, nextTick,  } from "vue";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const profile = ref(null);
 const isLoading = ref(true);
@@ -11,6 +15,29 @@ onMounted(async () => {
       endpoint: "profile",
     });
     profile.value = data;
+    await nextTick();
+    const cards = gsap.utils.toArray(".profile-card");
+    cards.forEach((card, index) => {
+      const isEven = index % 2 === 0;
+
+      gsap.set(card, {
+        alignSelf: isEven ? "flex-start" : "flex-end",
+        x: isEven ? -100 : 100,
+        opacity: 0,
+      });
+      gsap.to(card, {
+        x: 0,
+        opacity: 1,
+        duration: 1,
+        ease: "power2.out",
+        scrollTrigger: {
+          trigger: card,
+          start: "top 80%",
+          toggleActions: "play none none reverse"
+        }
+      });
+    });
+
   } catch (error) {
     console.error("データの取得に失敗しました", error);
   } finally {
