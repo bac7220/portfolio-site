2a1d83f03b441b1381975a2a80cbaa5ee90a3554
bac7220 <bacbac7220@gmail.com>
2025-12-31 13:51:45 +0900

    slotで呼び出せるようにコンポーネントを作成 ナビゲーションアローがでたり消えたりのアニメーション実装 配列でコードをスッキリさせるために調整開始

--- src/components/section/AboutSection.vue
+++ src/components/section/AboutSection.vue
@@ -1,4 +1,8 @@
 <script setup>
+import AboutArrow from "../icons/AboutArrow.vue";
+import ArrowLeft from "../icons/ArrowLeft.vue";
+import ArrowRight from "../icons/ArrowRight.vue";
+
 import { client } from "../../lib/microcms.js";
 import { ref, onMounted, nextTick, onUnmounted } from "vue";
 import { gsap } from "gsap";
@@ -30,53 +34,48 @@ onMounted(async () => {
         scrollTrigger: {
           trigger: ".profile-wrapper",
           start: "top top",
-          end: "+=50000",
-          scrub: true,
+          end: "+=20000",
+          scrub: 1.5,
           pin: true,
           // markers: true,
         }
       });
-      const firstImg = allContainer[0].querySelector(".profile-img");
-      if (firstImg) {
-        gsap.set(firstImg, { opacity: 1 })
+      const firstArrow = allContainer[0].querySelector(".card-arrow");
+      if (firstArrow) {
+        tl.fromTo(firstArrow, { opacity: 0 }, { opacity: 1, duration: 0.5 }, "<")
       }
 
       tl.to(allContainer[0], {
         x: "-100vw",
         y: "-100vh",
-        duration: 3,
+        duration: 10,
       });
 
 
       for (let i = 1; i < allContainer.length; i++) {
         const container = allContainer[i];
-        const img = container.querySelector(".profile-img");
+        const arrow = container.querySelector(".card-arrow");
         const xPos = i % 2 !== 0 ? "100vw" : "-100vw";
-
+        // プロフィールカードの動き制御
         tl.from(container, {
           x: xPos,
           y: "100vh",
-          duration: 2,
+          duration: 10,
         }, "<")
           .to(container, {
             x: 0,
             y: 0,
             duration: 1,
           });
-        if (img) {
-          tl.fromTo(img, { opacity: 0 }, { opacity: 1, duration: 0.5 }, "<");
-        }
-        if (i < allContainer.length - 1) {
-          if (img) {
-            tl.to(img, { opacity: 0, duration: 0.5 });
-          }
+        if (arrow) {
+          tl.fromTo(arrow, { opacity: 0 }, { opacity: 1, duration: 0.5 }, "<");
         }
 
         if (i < allContainer.length - 1) {
           tl.to(container, {
             x: xPos,
             y: "-100vh",
-            duration: 4,
+            duration: 10,
           });
         }
       }
@@ -105,50 +104,90 @@ onUnmounted(() => {
 
     <div v-if="profile" class="profile-wrapper">
       <!-- <pre>{{ profile }}</pre> -->
-      <div class="profile-container">
-        <div class="profile-card">
-          <h3 class="profile-label">名前</h3>
+      <div class="profile-container ">
+        <div class="profile-card row-container">
           <div class="card-content">
+            <h3 class="profile-label">名前</h3>
             <p>{{ profile?.profile_name }}</p>
             <img v-if="profile?.profile_img" class="profile-img" :src="profile?.profile_img?.url" alt="">
           </div>
           <div class="card-content">
             <h3 class="profile-label">Webでの活動名</h3>
             <p>ばく</p>
-            <img src="../../assets/image/BacIcon.webp" alt="">
+            <img src="../../assets/image/BacIcon.webp" alt="" class="profile-img">
           </div>
+        </div>
+        <div class="card-content">
+        </div>
+        <div class="card-arrow">
+          <AboutArrow>
+            <template #text>NEXT</template>
+            <template #after>
+              <ArrowRight />
+            </template>
+          </AboutArrow>
         </div>
       </div>
       <div class="profile-container">
         <div class="profile-card" v-html="profile?.profile_belief"></div>
         <img v-if="profile?.profile_img" class="profile-img" :src="profile?.profile_img?.url" alt="">
-
+        <AboutArrow>
+          <template #before>
+            <ArrowLeft />
+          </template>
+          <template #text>NEXT</template>
+        </AboutArrow>
       </div>
       <div class="profile-container">
         <div class="profile-card" v-html="profile?.profile_intro"></div>
         <img v-if="profile?.profile_img" class="profile-img" :src="profile?.profile_img?.url" alt="">
-
+        <AboutArrow>
+          <template #text>NEXT</template>
+          <template #after>
+            <ArrowRight />
+          </template>
+        </AboutArrow>
       </div>
 
       <div class="profile-container">
         <div class="profile-card">{{ profile?.profile_skill }}</div>
         <img v-if="profile?.profile_img" class="profile-img" :src="profile?.profile_img?.url" alt="">
-
+        <AboutArrow>
+          <template #before>
+            <ArrowLeft />
+          </template>
+          <template #text>NEXT</template>
+        </AboutArrow>
       </div>
       <div class="profile-container">
         <div class="profile-card">{{ profile?.profile_tools }}</div>
         <img v-if="profile?.profile_img" class="profile-img" :src="profile?.profile_img?.url" alt="">
-
+        <AboutArrow>
+          <template #text>NEXT</template>
+          <template #after>
+            <ArrowRight />
+          </template>
+        </AboutArrow>
       </div>
       <div class="profile-container">
         <div class="profile-card" v-html="profile?.profile_history"></div>
         <img v-if="profile?.profile_img" class="profile-img" :src="profile?.profile_img?.url" alt="">
-
+        <AboutArrow>
+          <template #before>
+            <ArrowLeft />
+          </template>
+          <template #text>NEXT</template>
+        </AboutArrow>
       </div>
       <div class="profile-container">
         <div class="profile-card" v-html="profile?.profile_activity"></div>
         <img v-if="profile?.profile_img" class="profile-img" :src="profile?.profile_img?.url" alt="">
-
+        <AboutArrow>
+          <template #text>NEXT</template>
+          <template #after>
+            <ArrowRight />
+          </template>
+        </AboutArrow>
       </div>
       <div class="profile-container">
         <div class="profile-card" v-html="profile?.profile_hobby"></div>
@@ -161,14 +200,13 @@ onUnmounted(() => {
   </div>
 </template>
 
+
+
 <style scoped>
 .about {
   overflow: hidden;
   width: 100%;
 }
-
-
-
 
 .profile-wrapper {
   position: relative;
@@ -188,6 +226,12 @@ onUnmounted(() => {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+.row-container {
+  display: flex;
+  flex-direction: row;
+  gap: 32px;
 }
 
 .profile-card {
@@ -210,19 +254,25 @@ onUnmounted(() => {
 
 .card-content {
   display: flex;
-  
+  flex-direction: column;
+  ;
 }
 
 .profile-card h3 {
-  width: 32px;
   font-weight: 700;
 }
 
 
 .profile-img {
+  position: absolute;
   max-width: 300px;
   height: auto;
   overflow: hidden;
+  opacity: 0;
+  z-index: -1;
+}
+
+.card-arrow {
   opacity: 0;
 }
 </style>
